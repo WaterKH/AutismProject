cd91f85b961545559d09b6789bc22598528bba00
waterkh <pjclark@email.sc.edu>
2016-01-27 15:05:25 -0500

    Drag and Drop Implemented
    DragScript and PlaceObject.cs have been implemented and are in use.

---
+++ Assets/GameScripts/PlaceObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlaceObject : MonoBehaviour {
+
+	public bool hovering;
+	public Color initialColor;
+	public Color finalColor;
+	public GameObject currObjectColor;
+	public bool switchColor;
+
+	void Awake()
+	{
+		initialColor = currObjectColor.GetComponent<RawImage>().color;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		currObjectColor.GetComponent<RawImage>().color = finalColor;
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if(!col.transform.GetComponent<DragScript>().isDragging)
+			col.GetComponent<CanvasGroup>().alpha = Mathf.Lerp(col.GetComponent<CanvasGroup>().alpha, 0, Time.deltaTime);
+
+		if(col.GetComponent<CanvasGroup>().alpha <= 0.05f)
+		{
+			col.gameObject.SetActive(false);
+			currObjectColor.GetComponent<RawImage>().color = initialColor;
+		}
+	}
+
+}
