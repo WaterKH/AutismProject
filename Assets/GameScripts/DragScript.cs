2a0cbe0960be4341973e9227562ab6887c88d896
waterkh <pjclark@email.sc.edu>
2016-01-27 13:31:20 -0500

    New Scene + Drag Functionality Complete
    Implemented the Drag function into the new scene. Next step will be to
    create colliders and then fade out the boxes if they set it down in the
    Place Here box.

---
+++ Assets/GameScripts/DragScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public GameObject objectMoving;
+	public bool isDragging;
+	private PointerEventData pointerData;
+	public Vector2 finalPosition;
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		objectMoving = eventData.pointerEnter;
+		if(objectMoving.tag == "Moveable")
+		{
+			//objectMoving.transform.position = eventData.position;
+			isDragging = true;
+			pointerData = eventData;
+		}
+		else
+		{
+			objectMoving = null;
+		}
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		//if(objectMoving.tag == "Moveable")
+		//	objectMoving.transform.position = Vector2.Lerp(objectMoving.transform.position, eventData.position, 
+		//												   Time.deltaTime * 2);
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+ 		//objectMoving = null;
+ 		isDragging = false;
+ 		pointerData = null;
+ 		finalPosition = eventData.position;
+	}
+
+	#endregion
+
+
+	void Update()
+	{
+		if(isDragging)
+		{
+			objectMoving.transform.position = Vector2.Lerp(objectMoving.transform.position, pointerData.position,
+														   Time.deltaTime * 10);
+		}
+		else if(objectMoving != null)
+		{
+			objectMoving.transform.position = Vector2.Lerp(objectMoving.transform.position, finalPosition, 
+														   Time.deltaTime * 10);
+		}
+	}
+
+}
